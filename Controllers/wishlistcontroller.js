b3ec62734517e2a8d9daf16ca4321159e53510eb
jlzimski <jlzimski@gmail.com>
2021-10-19 01:19:23 -0400

    wishlist controller updated 2 versions

--- Controllers/wishlistcontroller.js
+++ Controllers/wishlistcontroller.js
@@ -3,34 +3,43 @@ const router = Express.Router();
 const validateJWT = require("../middleware/validate-jwt");
 const { WishlistModel } = require("../models");
 
+// router.get('/practice', (req, res) => {
+//     res.send('Hey!! This is a practice route!')
+// });
+
+// router.get('/about', (req, res) => {
+//     res.send('This is the about route!')
+// });
+
 // ==============================
 //     Wishlist Create
 // ==============================
-router.post("/create", validateJWT, async (req, res) => {
-    const { name, description } = req.body.wishlist;
-    const { id } = req.user;
-    const { item_id } = req.item;
-    const wishlist = {
+router.post("/create", async (req, res) => {
+    const { name, item_id, description } = req.body.Wishlist;
+    const { id } = req.user.id;
+    const createWishlist = {
         name,
         owner: id,
-        item_id: id,
+        item_id,
         description
-    }
+    };
     try {
-        const newWishlist = await WishlistModel.create(wishlist);
+        const newWishlist = await WishlistModel.create(createWishlist);
         res.status(200).json(newWishlist);
     } catch (err) {
-        res.status(500).json({ error });
+        res.status(500).json({ error: err });
     }
 });
 // ===============================
 //      Get Wishlists by User
 // ===============================
 router.get("/mine", validateJWT, async (req, res) => {
-    const { id } = req.user;
+    const { id } = req.user.id;
     try {
         const userWishlists = await WishlistModel.findAll({
-            where: { owner: id }
+            where: {
+                owner: id
+            }
         });
         res.status(200).json(userWishlists);
     } catch (err) {
@@ -38,70 +47,185 @@ router.get("/mine", validateJWT, async (req, res) => {
     }
 });
 // ===============================
-//      Get Wishlist by Name
+//      Update a Wishlist
 // ===============================
-router.get("/:name", async (req, res) => {
-    const { name } = req.params;
+router.put("/update/:id", validateJWT, async (req, res) => {  // <---/:wishlistId/:itemId
+    const { name, description, itemId } = req.body.wishlist;
+    const wishlistId = req.params.wishlistId;
+    const userId = req.user.id;
+    const query = {
+        where: { id: wishlistId, owner: userId }
+    };
+    const updatedWishlist = {
+        name: name,
+        description: description,
+        itemId: itemId
+    };
+    try {
+        const update = await WishlistModel.update(updatedWishlist, query);
+        res.status(200).json(update);
+    } catch (err) {
+        res.status(500).json({ error: err });
+    }
+});
+// ===============================
+//      Delete a Wishlist
+// ===============================
+router.delete("/delete/:id", validateJWT, async (req, res) => {
+    const ownerId = req.user.id;
+    const wishlistId = req.params.id;
+
+    try {
+        const query = {
+            where: {
+                id: wishlistId,
+                owner: ownerId
+            }
+        };
+
+        await WishlistModel.destroy(query);
+        res.status(200).json({ message: "Wishlist Deleted" });
+    } catch (err) {
+        res.status(500).json({ error: err });
+    }
+});
+//============================================
+//      Delete Item from Wishlist         <---will this delete wishlist too?
+//============================================
+router.delete("/delete/:itemId", validateJWT, async (req, res) => {  //<---/:wishlistId/:itemId ???????
+    const wishlistId = req.wishlist.id;
+    const itemId = req.params.id;
+    try {
+        const query = {
+            where: {
+                id: wishlistId,
+                itemId: itemId
+            }
+        };
+        await WishlistModel.destroy(query);
+        res.status(200).json({ message: "Item Deleted" });
+    } catch (err) {
+        res.status(500).json({ error: err });
+    }
+});
+// ===============================
+//      Get Collection by Id
+// ===============================
+router.get("/:id", async (req, res) => {
+    const { id } = req.params;
     try {
         const results = await WishlistModel.findAll({
-            where: { name: name }
+            where: { id: id }
         });
         res.status(200).json(results);
     } catch (err) {
         res.status(500).json({ error: err });
     }
 });
-// ===============================
-//      Update a Wishlist
-// ===============================
-// router.put("/update/:wishlistId", validateJWT, async (req, res) => {
-//     const { name, description } = req.body.wishlist;
-//     const wishlistId = req.params.wishlistId;
-//     const owner = req.user.id;
 
-//     const query = {
-//         where: { id: wishlistId, owner: userId }
-//     };
+module.exports = router;
 
-//     const updatedWishlist = {
-//         name: name,
-//         description: description,
-//         item: item
-//     };
+const router = require("express").Router();
+const { Wishlist } = require("../models");
+const { jsonWebTokenError } = require("jsonwebtoken");
+const validateJWT = require("../middleware/validate-jwt");
 
-//     try {
-//         const update = await WishlistModel.update(updatedWishlist, query);
-//         res.status(200).json(update);
-//     } catch (err) {
-//         res.status(500).json({ error: err });
-//     }
-// });
-// ===============================
-//      Delete a Wishlist
-// ===============================
-// router.delete("/delete/:id", validateJWT, async (req, res) => {
-//     const ownerId = req.user.id;
-//     const wishlistId = req.params.id;
+router.post("/create", async (req, res) => {
+    const { name, item_id, description } = req.body.Wishlist;
+    const { id } = req.user.id;
+    const createWishlist = {
+        name,
+        owner: id,
+        item_id,
+        description
+    };
+    try {
+        const newWishlist = await Wishlist.create(createWishlist);
+        res.status(200).json(newWishlist);
+    } catch (err) {
+        res.status(500).json({ error: err });
+    }
+});
 
-//     try {
-//         const query = {
-//             where: {
-//                 id: wishlistId,
-//                 owner: ownerId
-//             }
-//         };
+router.get("/mine", validateJWT, async (req, res) => {
+    const { id } = req.user.id;
+    try {
+        const userWishlist = await Wishlist.findAll({
+            where: {
+                owner: id
+            }
+        });
+        res.status(200).json(userWishlist);
+    } catch (err) {
+        res.status(500).json({ error });
+    }
+});
 
-//         await WishlistModel.destroy(query);
-//         res.status(200).json({ message: "Wishlist Deleted" });
-//     } catch (err) {
-//         res.status(500).json({ error: err });
-//     }
-// });
+router.put("/update/:id", validateJWT, async (req, res) => {  // <---/:wishlistId/:itemId
+    const { name, description, itemId } = req.body.wishlist;
+    const wishlistId = req.params.wishlistId;
+    const userId = req.user.id;
+    const query = {
+        where: { id: wishlistId, owner: userId }
+    };
+    const updatedWishlist = {
+        name: name,
+        description: description,
+        itemId: itemId
+    };
+    try {
+        const update = await Wishlist.update(updatedWishlist, query);
+        res.status(200).json(update);
+    } catch (err) {
+        res.status(500).json({ error: err });
+    }
+});
 
+router.delete("/delete/:id", validateJWT, async (req, res) => {
+    const ownerId = req.user.id;
+    const wishlistId = req.params.id;
+    try {
+        const query = {
+            where: {
+                id: wishlistId,
+                owner: ownerId
+            }
+        };
+        await Wishlist.destroy(query);
+        res.status(200).json({ message: "Wishlist Deleted" });
+    } catch (err) {
+        res.status(500).json({ error: err });
+    }
+});
 
+//      Delete Item from Wishlist         <---will this delete wishlist too?
+router.delete("/delete/:itemId", validateJWT, async (req, res) => {  //<---/:wishlistId/:itemId ???????
+    const wishlistId = req.wishlist.id;
+    const itemId = req.params.id;
+    try {
+        const query = {
+            where: {
+                id: wishlistId,
+                itemId: itemId
+            }
+        };
+        await Wishlist.destroy(query);
+        res.status(200).json({ message: "Item Deleted" });
+    } catch (err) {
+        res.status(500).json({ error: err });
+    }
+});
 
-// router.get('/about', (req, res) => {
-//     res.send('This is the about route!')
-// });
+router.get("/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const results = await Wishlist.findAll({
+            where: { id: id }
+        });
+        res.status(200).json(results);
+    } catch (err) {
+        res.status(500).json({ error: err });
+    }
+});
 
 module.exports = router;
